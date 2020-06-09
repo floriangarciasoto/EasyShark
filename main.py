f7ed1600f5d15a6bfc6dec50ae41be8d80ec0f81
Garcia Florian <floriangarciasoto@gmail.com>
2020-06-09 19:19:30 +0200

    class

--- main.py
+++ main.py
@@ -39,8 +39,6 @@
 
 
 
-# --- Libraires ---
-
 import pyshark
 import tkinter as tk
 from tkinter import ttk
@@ -51,165 +49,154 @@ import os
 
 
 
-# --- Fonctions ---
+class EasyShark:
+	def __init__(self):
 
-def commencerStopperCapture():
-	global captureEnCours
-	if captureEnCours == False:
-		captureEnCours = True
-		nombreTrames['text'] = 'Aucune trame capturée, en attente de trafic ...'
-		boutonCommencerStopper['text'] = 'Stopper'
-		_thread.start_new_thread(capture,())
-	else:
-		captureEnCours = False
-		boutonCommencerStopper['text'] = 'Reprendre'
+		self.interfaces = list()
+		self.paquets = list()
 
-def changerInterface(event):
-	global interfaceCaptureEnCours
-	interfaceCaptureEnCours = interfaces[listeInterfaces.current()]
+		self.simplificationsInterfaces = {
+			'Câble' : ['enp','eno','eth'],
+			'Wifi' : ['wlp'],
+			'Loopback' : ['lo']
+		}
 
-def capture():
-	global numeroDerniereTrame
-	nombreTrames['text'] = 'En attente de trafic ... PING un peu !'
-	fenetre.update_idletasks()
-	for packet in pyshark.LiveCapture(interface=interfaceCaptureEnCours[0]).sniff_continuously(packet_count=10000):
-		if captureEnCours == False:
-			break
-		numeroDerniereTrame += 1
-		ligneTrame = str(numeroDerniereTrame)+'    Sur : '+interfaceCaptureEnCours[1]+'    '
-		explicationsTrame = 'Ouais en fait non.'
-		if 'eth' in packet:
-			ligneTrame += 'Ethernet    '
-			if packet.eth.type == '0x00000800':
-				ligneTrame += 'IPv4    '
-				if packet.ip.proto == '1':
-					ligneTrame += 'ICMP'
-					if packet.icmp.type == '8':
-						ligneTrame += ' PING'
-					if packet.icmp.type == '0':
-						ligneTrame += ' PONG'
-					ligneTrame += '    De %s    à    %s' % (packet.ip.src, packet.ip.dst)
-				elif packet.ip.proto == '6':
-					ligneTrame += 'TCP    '
-					if packet.tcp.dstport == '80':
-						ligneTrame += 'HTTP    '
-						if packet.tcp.flags_syn == '1':
-							ligneTrame += 'Connexion au site : %s' % packet.ip.dst
-						elif 'http' in packet:
-							if 'urlencoded-form' in packet:
-								ligneTrame += 'Regardes-moi le ce con il passe ses paramètres en clair :  '+arg(str(packet['urlencoded-form']))
-			elif packet.eth.type == '0x000086dd':
-				ligneTrame += 'IPv6     De %s    à    %s    (imbuvable.com)' % (packet.ipv6.src, packet.ipv6.dst)
-			elif packet.eth.type == '0x00000806':
-				ligneTrame += 'ARP ma gueule !'
-			else:
-				ligneTrame += 'IPBXv4'
+		for nomSystemeInterface in os.popen('ip a | grep ^[0-9]*: | cut -d" " -f 2 | sed "s/.$//g"').read().split("\n")[:-1]:
+			self.interfaces.append([nomSystemeInterface,'Inconnue ('+nomSystemeInterface+')'])
+
+		for i in range(len(self.interfaces)):
+			for j in self.simplificationsInterfaces:
+				for n in self.simplificationsInterfaces[j]:
+					if self.interfaces[i][0][:len(n)] == n:
+						self.interfaces[i][1] = j
+
+		self.interfaceCaptureEnCours = self.interfaces[0]
+		self.indexInterfaceCaptureEnCours = 0
+		for i in range(len(self.interfaces)):
+			if int(os.popen('ip a s '+self.interfaces[i][0]+' | head -n 1 | grep "state UP" | wc -l').read()) == 1:
+				self.interfaceCaptureEnCours = self.interfaces[i]
+				self.indexInterfaceCaptureEnCours = i
+
+		self.captureEnCours = False
+		self.numeroDerniereTrame = 0
+
+		self.listeInterfacesValeurs = list()
+		for i in self.interfaces:
+			self.listeInterfacesValeurs.append(i[1])
+
+
+		self.fenetre = tk.Tk()
+
+		self.fenetre.title('EasyShark')
+
+		Label(self.fenetre, text='Commences pas choisir l\'interface : ').grid(row=0, column=0, sticky=E)
+
+		self.listeInterfaces = ttk.Combobox(self.fenetre, values=self.listeInterfacesValeurs)
+		self.listeInterfaces.current(self.indexInterfaceCaptureEnCours)
+		self.listeInterfaces.grid(row=0, column=1, columnspan=2, sticky=W)
+		self.listeInterfaces.bind("<<ComboboxSelected>>", self.changerInterface)
+
+		self.boutonCommencerStopper = tk.Button(self.fenetre, text='Capturer', command=self.commencerStopperCapture)
+		self.boutonCommencerStopper.grid(row=1,column=0, sticky=E)
+
+		tk.Button(self.fenetre, text='Réiniatiliser', command=self.reinitialiserCapture).grid(row=1, column=1, sticky=W)
+
+		Label(self.fenetre, text='Liste des trames :').grid(row=2, columnspan=2)
+
+		self.listeTrames = Listbox(self.fenetre, height=20, width=100)
+		self.listeTrames.bind('<<ListboxSelect>>', self.clickSurTrame)
+		self.listeTrames.grid(row=3, columnspan=2)
+
+		self.nombreTrames = Label(self.fenetre, text='Faut appuyer sur Capturer en fait')
+		self.nombreTrames.grid(row=4, columnspan=2)
+
+		Label(self.fenetre, text='J\'t\'explique : ').grid(row=5, column=0)
+		self.champExplicationsTrame = ScrolledText(self.fenetre, height=10, width=50)
+		self.champExplicationsTrame.grid(row=6, column=0)
+
+		Label(self.fenetre, text='Détails imbuvables, regardes pas si t\'es une âme sensible :').grid(row=5, column=1)
+		self.detailsTrame = ScrolledText(self.fenetre, height=10, width=50)
+		self.detailsTrame.grid(row=6, column=1)
+
+		self.fenetre.mainloop()
+
+
+	def commencerStopperCapture(self):
+		if self.captureEnCours == False:
+			self.captureEnCours = True
+			self.nombreTrames['text'] = 'Aucune trame capturée, en attente de trafic ...'
+			self.boutonCommencerStopper['text'] = 'Stopper'
+			_thread.start_new_thread(self.capture,())
 		else:
-			ligneTrame += 'Ah ben là C compliqué    '
-		listeTrames.insert(END, ligneTrame)
-		paquets.append([explicationsTrame,'Trame %d\n%s' % (numeroDerniereTrame,packet)])
-		nombreTrames['text'] = 'Trames capturées : %d' % numeroDerniereTrame
-		fenetre.update_idletasks()
+			self.captureEnCours = False
+			self.boutonCommencerStopper['text'] = 'Reprendre'
 
-def reinitialiserCapture():
-	global paquets
-	global numeroDerniereTrame
-	listeTrames.delete(0,len(paquets)-1)
-	numeroDerniereTrame = 0
-	paquets = list()
-	champExplicationsTrame.delete(1.0,END)
-	detailsTrame.delete(1.0,END)
-	nombreTrames['text'] = 'En attente de trafic à nouveau ...'
-	if captureEnCours == False:
-		nombreTrames['text'] += ' (faut appuyer sur Reprende en fait)'
+	def changerInterface(self, event):
+		self.interfaceCaptureEnCours = self.interfaces[self.listeInterfaces.current()]
 
-def clickSurTrame(event):
-	if len(event.widget.curselection()) > 0:
-		champExplicationsTrame.delete(1.0,END)
-		champExplicationsTrame.insert(END, '%s' % paquets[int(event.widget.curselection()[0])][0])
-		detailsTrame.delete(1.0,END)
-		detailsTrame.insert(END, '%s' % paquets[int(event.widget.curselection()[0])][1])
-		fenetre.update_idletasks()
+	def capture(self):
+		self.nombreTrames['text'] = 'En attente de trafic ... PING un peu !'
+		self.fenetre.update_idletasks()
+		for trame in pyshark.LiveCapture(interface=self.interfaceCaptureEnCours[0]).sniff_continuously(packet_count=10000):
+			if self.captureEnCours == False:
+				break
+			self.numeroDerniereTrame += 1
+			ligneTrame = str(self.numeroDerniereTrame)+'    Sur : '+self.interfaceCaptureEnCours[1]+'    '
+			explicationsTrame = 'Ouais en fait non.'
+			if 'eth' in trame:
+				ligneTrame += 'Ethernet    '
+				if trame.eth.type == '0x00000800':
+					ligneTrame += 'IPv4    '
+					if trame.ip.proto == '1':
+						ligneTrame += 'ICMP'
+						if trame.icmp.type == '8':
+							ligneTrame += ' PING'
+						if trame.icmp.type == '0':
+							ligneTrame += ' PONG'
+						ligneTrame += '    De %s    à    %s' % (trame.ip.src, trame.ip.dst)
+					elif trame.ip.proto == '6':
+						ligneTrame += 'TCP    '
+						if trame.tcp.dstport == '80':
+							ligneTrame += 'HTTP    '
+							if trame.tcp.flags_syn == '1':
+								ligneTrame += 'Connexion au site : %s' % trame.ip.dst
+							elif 'http' in trame:
+								if 'urlencoded-form' in trame:
+									ligneTrame += 'Regardes-moi le ce con il passe ses paramètres en clair :  '
+									argumentsHTTP = str(trame['urlencoded-form']).split('\n')[1:]
+									for i in range(0,len(argumentsHTTP)-1,3):
+										ligneTrame += argumentsHTTP[i+1][6:]+' : '+argumentsHTTP[i+2][8:]+', '
+				elif trame.eth.type == '0x000086dd':
+					ligneTrame += 'IPv6     De %s    à    %s    (imbuvable.com)' % (trame.ipv6.src, trame.ipv6.dst)
+				elif trame.eth.type == '0x00000806':
+					ligneTrame += 'ARP ma gueule !'
+				else:
+					ligneTrame += 'IPBXv4'
+			else:
+				ligneTrame += 'Ah ben là C compliqué    '
+			self.listeTrames.insert(END, ligneTrame)
+			self.paquets.append([explicationsTrame,'Trame %d\n%s' % (self.numeroDerniereTrame,trame)])
+			self.nombreTrames['text'] = 'Trames capturées : %d' % self.numeroDerniereTrame
+			self.fenetre.update_idletasks()
 
-def arg(packet):
-	packet = packet.split('\n')
-	packet.pop(0)
-	tx = ''
-	for i in range(0,len(packet)-1,3):
-		tx += packet[i+1][6:]+' : '+packet[i+2][8:]+', '
-	return tx[:-2]
+	def reinitialiserCapture(self):
+		self.listeTrames.delete(0,len(self.paquets)-1)
+		self.numeroDerniereTrame = 0
+		self.paquets = list()
+		self.champExplicationsTrame.delete(1.0,END)
+		self.detailsTrame.delete(1.0,END)
+		self.nombreTrames['text'] = 'En attente de trafic à nouveau ...'
+		if self.captureEnCours == False:
+			self.nombreTrames['text'] += ' (faut appuyer sur Reprende en fait)'
 
-
-
-# --- Initialisation d'EasyShark ---
-
-interfaces = list()
-paquets = list()
-
-simplificationsInterfaces = {
-	'Câble' : ['enp','eno','eth'],
-	'Wifi' : ['wlp'],
-	'Loopback' : ['lo']
-}
-
-for nomSystemeInterface in os.popen('ip a | grep ^[0-9]*: | cut -d" " -f 2 | sed "s/.$//g"').read().split("\n")[:-1]:
-	interfaces.append([nomSystemeInterface,'Inconnue ('+nomSystemeInterface+')'])
-
-for i in range(len(interfaces)):
-	for j in simplificationsInterfaces:
-		for n in simplificationsInterfaces[j]:
-			if interfaces[i][0][:len(n)] == n:
-				interfaces[i][1] = j
-
-interfaceCaptureEnCours = interfaces[0]
-indexInterfaceCaptureEnCours = 0
-for i in range(len(interfaces)):
-	if int(os.popen('ip a s '+interfaces[i][0]+' | head -n 1 | grep "state UP" | wc -l').read()) == 1:
-		interfaceCaptureEnCours = interfaces[i]
-		indexInterfaceCaptureEnCours = i
-
-captureEnCours = False
-numeroDerniereTrame = 0
-
-listeInterfacesValeurs = list()
-for i in interfaces:
-	listeInterfacesValeurs.append(i[1])
-
+	def clickSurTrame(self, event):
+		if len(event.widget.curselection()) > 0:
+			self.champExplicationsTrame.delete(1.0,END)
+			self.champExplicationsTrame.insert(END, '%s' % self.paquets[int(event.widget.curselection()[0])][0])
+			self.detailsTrame.delete(1.0,END)
+			self.detailsTrame.insert(END, '%s' % self.paquets[int(event.widget.curselection()[0])][1])
+			self.fenetre.update_idletasks()
 
 
-# --- Création de la fenêtre ---
-
-fenetre = tk.Tk()
-
-fenetre.title('EasyShark')
-
-Label(fenetre, text='Commences pas choisir l\'interface : ').grid(row=0, column=0, sticky=E)
-
-listeInterfaces = ttk.Combobox(fenetre, values=listeInterfacesValeurs)
-listeInterfaces.current(indexInterfaceCaptureEnCours)
-listeInterfaces.grid(row=0, column=1, columnspan=2, sticky=W)
-listeInterfaces.bind("<<ComboboxSelected>>", changerInterface)
-
-boutonCommencerStopper = tk.Button(fenetre, text='Capturer', command=commencerStopperCapture)
-boutonCommencerStopper.grid(row=1,column=0, sticky=E)
-
-tk.Button(fenetre, text='Réiniatiliser', command=reinitialiserCapture).grid(row=1, column=1, sticky=W)
-
-Label(fenetre, text='Liste des trames :').grid(row=2, columnspan=2)
-
-listeTrames = Listbox(fenetre, height=20, width=100)
-listeTrames.bind('<<ListboxSelect>>', clickSurTrame)
-listeTrames.grid(row=3, columnspan=2)
-
-nombreTrames = Label(fenetre, text='Faut appuyer sur Capturer en fait.')
-nombreTrames.grid(row=4, columnspan=2)
-
-Label(fenetre, text='J\'t\'explique : ').grid(row=5, column=0)
-champExplicationsTrame = ScrolledText(fenetre, height=10, width=50)
-champExplicationsTrame.grid(row=6, column=0)
-
-Label(fenetre, text='Détails imbuvables, regardes pas si t\'es une âme sensible :').grid(row=5, column=1)
-detailsTrame = ScrolledText(fenetre, height=10, width=50)
-detailsTrame.grid(row=6, column=1)
-
-tk.mainloop()
+if __name__ == '__main__':
+	EasyShark()
