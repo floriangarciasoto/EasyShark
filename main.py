2aff5a655f785ba83d0b2571584dca92599ca047
Garcia Florian <floriangarciasoto@gmail.com>
2020-06-09 02:01:38 +0200

    explications trame v1

--- main.py
+++ main.py
@@ -68,37 +68,38 @@ def capture():
 		if captureEnCours == False:
 			break
 		numeroDerniereTrame += 1
-		trame = str(numeroDerniereTrame)+'    Sur : '+interfaceCaptureEnCours[1]+'    '
+		ligneTrame = str(numeroDerniereTrame)+'    Sur : '+interfaceCaptureEnCours[1]+'    '
+		explicationsTrame = 'Ouais en fait non.'
 		if 'eth' in packet:
-			trame += 'Ethernet    '
+			ligneTrame += 'Ethernet    '
 			if packet.eth.type == '0x00000800':
-				trame += 'IPv4    '
+				ligneTrame += 'IPv4    '
 				if packet.ip.proto == '1':
-					trame += 'ICMP'
+					ligneTrame += 'ICMP'
 					if packet.icmp.type == '8':
-						trame += ' PING'
+						ligneTrame += ' PING'
 					if packet.icmp.type == '0':
-						trame += ' PONG'
-					trame += '    De %s    à    %s' % (packet.ip.src, packet.ip.dst)
+						ligneTrame += ' PONG'
+					ligneTrame += '    De %s    à    %s' % (packet.ip.src, packet.ip.dst)
 				elif packet.ip.proto == '6':
-					trame += 'TCP    '
+					ligneTrame += 'TCP    '
 					if packet.tcp.dstport == '80':
-						trame += 'HTTP    '
+						ligneTrame += 'HTTP    '
 						if packet.tcp.flags_syn == '1':
-							trame += 'Connexion au site : %s' % packet.ip.dst
+							ligneTrame += 'Connexion au site : %s' % packet.ip.dst
 						elif 'http' in packet:
 							if 'urlencoded-form' in packet:
-								trame += 'Regardes-moi le ce con il passe ses paramètres en clair :  '+arg(str(packet['urlencoded-form']))
+								ligneTrame += 'Regardes-moi le ce con il passe ses paramètres en clair :  '+arg(str(packet['urlencoded-form']))
 			elif packet.eth.type == '0x000086dd':
-				trame += 'IPv6     De %s    à    %s    (imbuvable.com)' % (packet.ipv6.src, packet.ipv6.dst)
+				ligneTrame += 'IPv6     De %s    à    %s    (imbuvable.com)' % (packet.ipv6.src, packet.ipv6.dst)
 			elif packet.eth.type == '0x00000806':
-				trame += 'ARP ma gueule !'
+				ligneTrame += 'ARP ma gueule !'
 			else:
-				trame += 'IPBXv4'
+				ligneTrame += 'IPBXv4'
 		else:
-			trame += 'Ah ben là C compliqué    '
-		listeTrames.insert(END, trame)
-		paquets.append('Trame %d\n%s' % (numeroDerniereTrame,packet))
+			ligneTrame += 'Ah ben là C compliqué    '
+		listeTrames.insert(END, ligneTrame)
+		paquets.append([explicationsTrame,'Trame %d\n%s' % (numeroDerniereTrame,packet)])
 		nombre['text'] = 'Trames capturées : %d' % numeroDerniereTrame
 		fenetre.update_idletasks()
 
@@ -116,8 +117,10 @@ def reinitialiserCapture():
 
 def clicktrame(event):
 	if len(event.widget.curselection()) > 0:
-		details.delete(1.0,END)
-		details.insert(END, '%s' % paquets[int(event.widget.curselection()[0])])
+		champExplicationsTrame.delete(1.0,END)
+		champExplicationsTrame.insert(END, '%s' % paquets[int(event.widget.curselection()[0])][0])
+		detailsTrame.delete(1.0,END)
+		detailsTrame.insert(END, '%s' % paquets[int(event.widget.curselection()[0])][1])
 		fenetre.update_idletasks()
 
 def arg(packet):
@@ -172,31 +175,36 @@ fenetre = tk.Tk()
 
 fenetre.title('EasyShark')
 
-Label(fenetre, text='Commences pas choisir l\'interface : ').grid(row=0, column=0, columnspan=2)
+Label(fenetre, text='Commences pas choisir l\'interface : ').grid(row=0, column=0, sticky=E)
 
 listeInterfaces = ttk.Combobox(fenetre, values=listeInterfacesValeurs)
 listeInterfaces.current(indexInterfaceCaptureEnCours)
-listeInterfaces.grid(row=0, column=1, columnspan=2)
+listeInterfaces.grid(row=0, column=1, columnspan=2, sticky=W)
 listeInterfaces.bind("<<ComboboxSelected>>", changerInterface)
 
 tk.Button(fenetre, text='Capturer', command=commencerCapture).grid(row=1,column=0, sticky=E)
 
-tk.Button(fenetre, text='Stopper', command=stopperCapture).grid(row=1,column=1)
+tk.Button(fenetre, text='Réiniatiliser', command=reinitialiserCapture).grid(row=1, column=1, sticky=W)
 
-tk.Button(fenetre, text='Réiniatiliser', command=reinitialiserCapture).grid(row=1,column=2, sticky=W)
-
-Label(fenetre, text='Liste des trames :').grid(row=2, columnspan=3)
+Label(fenetre, text='Liste des trames :').grid(row=2, columnspan=2)
 
 listeTrames = Listbox(fenetre, height=20, width=100)
 listeTrames.bind('<<ListboxSelect>>', clicktrame)
-listeTrames.grid(row=3, columnspan=3)
+listeTrames.grid(row=3, columnspan=2)
 
 nombre = Label(fenetre, text='Aucune trame capturée, en attente de trafic ...')
-nombre.grid(row=4, columnspan=3)
+nombre.grid(row=4, columnspan=2)
 
-Label(fenetre, text='Détails imbuvables, regardes pas si t\'es une âme sensible :').grid(row=5, columnspan=3)
 
-details = ScrolledText(fenetre, height=10, width=100)
-details.grid(row=6, columnspan=3)
+Label(fenetre, text='J\'t\'explique : ').grid(row=5, column=0)
+
+champExplicationsTrame = ScrolledText(fenetre, height=10, width=50)
+champExplicationsTrame.grid(row=6, column=0)
+
+
+Label(fenetre, text='Détails imbuvables, regardes pas si t\'es une âme sensible :').grid(row=5, column=1)
+
+detailsTrame = ScrolledText(fenetre, height=10, width=50)
+detailsTrame.grid(row=6, column=1)
 
 tk.mainloop()
